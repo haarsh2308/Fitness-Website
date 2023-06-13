dd29ae75583dfd6db06150b3883416073737e955
Harsh Talwar <htalwar2308@gmail.com>
2023-06-13 16:06:37 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,15 @@
       return document.querySelector(el)
     }
   }
+// form submition
+
+  const form = document.querySelector('form');
+const thankYouMessage = document.querySelector('#thank-you-message');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  thankYouMessage.classList.add('show');
+  setTimeout(() => form.submit(), 2000);
+});
 
   /**
    * Easy event listener function
